d4bbac1654994083c637238612e3ea2223321b7f
Aaron Soria <asoria@silentiumapps.com>
2019-08-30 13:10:56 -0300

    Service completamente implementado en Android, falta volver al home en IOs

--- MercadoPagoCrossPlatform/MercadoPagoCrossPlatform.Android/Service/MercadoPagoService.cs
+++ MercadoPagoCrossPlatform/MercadoPagoCrossPlatform.Android/Service/MercadoPagoService.cs
@@ -2,6 +2,8 @@
 using Android.App;
 using Android.Content;
 using Com.Mercadopago.Android.PX.Core;
+using Com.Mercadopago.Android.PX.Model;
+using MercadoPagoCrossPlatform;
 using MercadoPagoCrossPlatform.Droid.Service;
 using MercadoPagoCrossPlatform.Interfaces;
 using Xamarin.Forms;
@@ -11,7 +13,7 @@ namespace MercadoPagoCrossPlatform.Droid.Service
 {
     public class MercadoPagoService : IMercadoPagoService
     {
-        private EventHandler OnPaymentResult;
+        private static EventHandler OnPaymentResult;
         private static Context Context;
         private static MercadoPagoCheckout mercadoPagoCheckout;
 
@@ -26,15 +28,48 @@ namespace MercadoPagoCrossPlatform.Droid.Service
         {
             if (requestCode == MercadoPagoCheckout.PaymentResultCode)
             {
-                var result = data.GetSerializableExtra(MercadoPagoCheckout.ExtraPaymentResult);
+                var result = (Payment)data.GetSerializableExtra(MercadoPagoCheckout.ExtraPaymentResult);
+                MercadoPagoCrossPlatform.Support.MercadoPagoPaymentResponse response = null;
                 if ((int)resultCode == 7)
                 {
-                    
+                    if (result != null)
+                    {
+                        response = new MercadoPagoCrossPlatform.Support.MercadoPagoPaymentResponse
+                        {
+                            PaymentId = result.Id.ToString(),
+                            PaymentDescription = result.PaymentStatusDetail
+                        };
+
+                        switch (result.PaymentStatus)
+                        {
+                            case "approved":
+                                response.PaymentStatus = MercadoPagoCrossPlatform.Support.PaymentStatus.APPROVED;
+                                break;
+                            case "in_process":
+                                response.PaymentStatus = MercadoPagoCrossPlatform.Support.PaymentStatus.INPROCESS;
+                                break;
+                            case "rejected":
+                                response.PaymentStatus = MercadoPagoCrossPlatform.Support.PaymentStatus.REJECTED;
+                                break;
+                            case "pending":
+                                response.PaymentStatus = MercadoPagoCrossPlatform.Support.PaymentStatus.PENDING;
+                                break;
+                            case "cancelled":
+                                response.PaymentStatus = MercadoPagoCrossPlatform.Support.PaymentStatus.CANCELLED;
+                                break;
+                            case "expired":
+                                response.PaymentStatus = MercadoPagoCrossPlatform.Support.PaymentStatus.EXPIRED;
+                                break;
+                        }
+                    }
+
                 }
                 else
                 {
 
                 }
+
+                OnPaymentResult?.Invoke(response, EventArgs.Empty);
             }   
         }
 
